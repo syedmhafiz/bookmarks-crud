132a3a079645e6f3a8b08567d70ea7d7a0068260
syedmhafiz <syedhafiz61@gmail.com>
2025-04-25 08:50:15 +0600

    V1.4: Added JWT authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,12 @@ import { ForbiddenException, Injectable, InternalServerErrorException } from '@n
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) {}
 
     async signUp(dto: AuthDto) {
         try {
@@ -28,13 +29,12 @@ export class AuthService {
                 }
             });
 
-            delete user.hash;
-            return user;
+            return this.signToken(user?.id, user?.email);
         } catch (error) {
             if (error instanceof ForbiddenException) throw error;
             throw new InternalServerErrorException('Failed to create user');
         }
-      }
+    }
 
     async logIn(dto: AuthDto) {
         // find the user
@@ -53,7 +53,22 @@ export class AuthService {
             'Incorrect password, try again'
         );
 
-        delete user.hash;
-        return user;
+        return this.signToken(user?.id, user?.email);
+    }
+
+    async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email,
+        }
+
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '60m',
+            secret: this.config.get('JWT_SECRET'),
+        });
+
+        return {
+            access_token: token
+        };
     }
 }
